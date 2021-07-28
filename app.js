333c153950f6c9645e3c97db787deb43cc9e7f4b
Visal Yun <yunvisal18@kit.edu.kh>
2021-07-28 20:48:35 +0700

    add .env file and modifies post /book parameter

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const session = require('express-session');
-const redis = require('redis')
-const connectRedis = require('connect-redis')
-const passport = require('passport');
 const multer = require('multer')
-const genPassword = require('./lib/passwordUtil').genPassword;
-const { sequelize, Book, Chapters, User } = require('./models')
+const { sequelize, Book, Chapters } = require('./models')
 
 const app = express()
 app.use(express.json())
@@ -38,104 +33,14 @@ const upload = multer({
     fileFilter: fileFilter
 });
 
-/* ------------------ Session Config ------------------ */
-const RedisStore = connectRedis(session)
-//Configure redis client
-const redisClient = redis.createClient({
-    host: 'localhost',
-    port: 6379
-})
-redisClient.on('error', err => {
-    console.log('Could not establish a connection with redis. ' + err);
-});
-redisClient.on('connect', err => {
-    console.log('Connected to redis successfully');
-});
-
-//Configure session middleware
-app.use(session({
-    secret: 'secret$%^134',
-    resave: false,
-    saveUninitialized: true,
-    store: new RedisStore({ client: redisClient }),
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
-    }
-}))
-
-/* ------------------ Passport Config ------------------ */
-require('./config/passport');
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use((req, res, next) => {
-    console.log(req.session);
-    console.log(req.user);
-    next();
-})
-
-/* ------------------ Auth route ------------------ */
-app.post("/register", async(req, res) => {
-    const { username, password } = req.body
-    
-    const saltHash = genPassword(password);
-    const salt = saltHash.salt;
-    const hash = saltHash.hash;
-    
-    await User.findOne({where: {username}})
-        .then(async user => {
-            if(user) { return res.json("Username is already taken") }
-
-            try {
-                const user = await User.create({ username, salt, hash })
-                return res.json(user)
-            } catch (e) {
-                console.log(e)
-                return res.status(500).json(e)
-            }
-        })
-        .catch((e) => {
-            console.log(e)
-            return res.status(500).json(e)
-        });
-})
-
-app.post("/login", passport.authenticate('local', { failureRedirect: '/login-failure', successRedirect: '/login-success' }));
-
-app.get("/logout", (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return console.log(err);
-        }
-        return res.json("Logout success!")
-    });
-});
-
-app.get("/login-success", (req, res) => {
-    return res.json("Login successful")
-})
-
-app.get("/login-failure", (req, res) => {
-    return res.json("Login fail! Please check your login credential again")
-})
-
-const isAuth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.status(401).json({ msg: 'You are not authorized to view this resource' });
-    }
-}
-
 /* ------------------ Book Route ------------------ */
 app.post("/book", upload.single('book_file'), async(req, res) => {
-    const { title, author, published_date, description, thumbnail_url } = req.body
+    const { title, author, published_date, description, donator, donator_phone_number } = req.body
     console.log(req.file)
     const book_file = req.file.path
 
     try{
-        const book = await Book.create({ title, author, published_date, description, thumbnail_url, book_file })
+        const book = await Book.create({ title, author, published_date, description, book_file, donator, donator_phone_number })
         return res.json(book)
     }catch(e){
         console.log(e)
@@ -143,7 +48,7 @@ app.post("/book", upload.single('book_file'), async(req, res) => {
     }
 })
 
-app.get("/book", isAuth, async (req, res) => {
+app.get("/book", async (req, res) => {
     try {
         const book = await Book.findAll()
         return res.json(book)
--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "{YOUR_USERNAME}",
-    "password": "{YOUR_PASSWORD}",
-    "database": "{YOUR_DATABASE}",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
